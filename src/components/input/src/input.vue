7b22d848624a476965462a0298fb4228257820a0
wangying <leosmilence@163.com>
2019-04-09 21:01:24 +0800

    修改文本框样式

--- src/components/input/src/input.vue
+++ src/components/input/src/input.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="pinput">
-    <div class="per-inputunitL">星期：</div>
-    <div class="per-inputunit">kWh</div>
-    <div class="per-inputwrapR">
-      <input type="text" text placeholder="请输入">
+  <div class="pinput" :class="'default'">
+    <div class="per-text left" v-if="false">星期：</div>
+    <div class="per-center">
+      <input ref="input" @focus="focusHandler" type="text" placeholder="请输入">
       <div class="per-inputborder"></div>
     </div>
+    <div class="per-text right" v-if="false">kWh</div>
+    {{focus}}
     <div class="reminder-tip" friendly>
       <em></em>
     </div>
@@ -17,37 +18,71 @@
 
 <style>
 .pinput {
-  position: relative;
+  width: 245px;
+  display: flex;
   height: 30px;
   line-height: 30px;
   background: #fff;
+  flex-flow: row nowrap;
+  align-items: stretch;
+  justify-content: space-around;
+  font-size: 14px;
 }
 
-.pinput .per-inputunit {
-  float: right;
+.pinput .per-center {
+  height: 30px;
+  line-height: 30px;
+  flex: 1;
+}
+
+.pinput .per-text {
+  position: relative;
   color: #5478a4;
   box-sizing: border-box;
   padding: 0 10px;
-  height: 100%;
-  position: relative;
-  z-index: 2;
-  align-items: center;
-  display: flex;
 }
 
-.pinput .per-inputwrap {
-  line-height: 30px;
-  overflow: hidden;
-  height: 100%;
+.pinput .per-text.left::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  border-left: 1px solid #ccc;
+  height: 16px;
+  right: 0;
+  margin-top: -8px;
+}
+.pinput .per-text.right::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  border-left: 1px solid #ccc;
+  height: 16px;
+  left: 0;
+  margin-top: -8px;
 }
 
-.pinput input[type="text"],
-input[type="password"],
-textarea {
-  border-color: #cacaca;
-  border-width: 1px;
-  border-style: solid;
-  background: #fff;
+.default {
+  border: 1px solid #cacaca;
+}
+
+.focus {
+  border: 1px solid #02a9d1;
+}
+
+.error {
+  border: 1px solid #f97c7c;
+}
+
+.pinput .per-center input[type="text"] {
+  color: #333;
+  height: 30px;
+  padding: 0 8px;
+  width: 100%;
+  line-height: 28px;
+  box-sizing: border-box;
+  margin: 0;
+  border: none;
+  outline: none;
 }
 </style>
 
@@ -55,6 +90,28 @@ textarea {
 
 <script>
 export default {
-  name: "pinput"
+  name: "pinput",
+  data() {
+    return {
+      focus: false
+    };
+  },
+  props: {
+    width: {
+      type: Number,
+      default: () => 245
+    }
+  },
+  methods: {
+    focusHandler() {
+      let bool = this.$emit("focus");
+      if (bool === false) return;
+      this.focus = true;
+    },
+    blurHandler() {
+      // let bo
+    }
+  },
+  computed: {}
 };
 </script>
